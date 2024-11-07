1bce8fa6e3bafe3a8ff722daf91d1f597faad73e
vanshsharma-tech <vanshsharma9431@gmail.com>
2024-11-07 20:24:21 +0530

    Updated files with new chages

--- Dom/01/script.js
+++ Dom/01/script.js
@@ -2,4 +2,14 @@
 
 let h1 = document.getElementsByTagName("h1")[0];
 console.log(h1.innerText)
-h1.innerText = "Hello, Duniya!";
+h1.innerText = "Hello, Duniya!";  // Here i change the text of Hello World 0 to this
+
+let anotherH1 = document.getElementsByTagName("h1")[1];
+console.log(anotherH1.innerText)
+anotherH1.style.color="red"
+
+// Here i am trying to access the value which is present in div container
+
+let div = document.getElementsByTagName("div")[2];
+console.log(div.innerText)  
+div.style.backgroundColor="aqua"
