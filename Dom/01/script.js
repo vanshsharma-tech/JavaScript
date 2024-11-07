9d0e7a97ceabf40fbb50053978b8ee19b6a9be7d
vanshsharma-tech <vanshsharma9431@gmail.com>
2024-11-07 19:48:11 +0530

    Second Commit

---
+++ Dom/01/script.js
@@ -0,0 +1,5 @@
+// How to access the tag element in javascipt by using getElementsByTagName
+
+let h1 = document.getElementsByTagName("h1")[0];
+console.log(h1.innerText)
+h1.innerText = "Hello, Duniya!";
